3c9c928a60d2db4be06d3d40cead60881ea4458f
Michael <mpeterson2@gmail.com>
2012-05-06 15:06:59 -0400

    Koan05 completed and worked. Needs to be stripped.

---
+++ completed-koans/koan05/Koan05.java
@@ -0,0 +1,129 @@
+package net.thornydev.mybatis.koan.koan05;
+
+import static org.junit.Assert.*;
+
+import java.io.InputStream;
+import java.util.List;
+
+import net.thornydev.mybatis.koan.Country;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Koan05 {
+
+	static SqlSessionFactory sessionFactory;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		String resource = "net/thornydev/mybatis/koan/koan05/koan05-config.xml";  
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		sessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		inputStream.close();
+	}
+
+	@Test
+	public void learnToQueryForTotalCount() throws Exception {
+		// TODO: to move to the more idiomatic way of using sessions, create
+		//       a session here and use a try/finally to close it when done
+		SqlSession session = null;
+		try {
+			session = sessionFactory.openSession();
+			Koan05Mapper mapper = session.getMapper(Koan05Mapper.class);
+			int totalCountries = mapper.getCountryCount();
+			
+			assertEquals(109, totalCountries);
+			
+		} finally {
+			if (session != null) session.close();
+		}
+	}
+
+	@Test
+	public void learnToQueryWithMultipleParams() throws Exception {
+		// TODO: to move to the more idiomatic way of using sessions, create
+		//       a session here and use a try/finally to close it when done
+		SqlSession session = null;
+		try {
+			session = sessionFactory.openSession();
+			Koan05Mapper mapper = session.getMapper(Koan05Mapper.class);
+			List<Country> lc = mapper.getCountryRange(22, 33);
+			
+			assertEquals(12, lc.size());
+			Country finland = lc.get(11);
+			assertEquals("Finland", finland.getCountry());
+			
+		} finally {
+			if (session != null) session.close();
+		}
+	}	
+	
+	@Test
+	public void learnToQueryWithMultipleParamsWithAnnotatedNames() throws Exception {
+		// TODO: to move to the more idiomatic way of using sessions, create
+		//       a session here and use a try/finally to close it when done
+		SqlSession session = null;
+		try {
+			session = sessionFactory.openSession();
+			Koan05Mapper mapper = session.getMapper(Koan05Mapper.class);
+			List<Country> lc = mapper.getCountryRange2(22, 33);
+			
+			assertEquals(12, lc.size());
+			Country finland = lc.get(11);
+			assertEquals("Finland", finland.getCountry());
+			
+		} finally {
+			if (session != null) session.close();
+		}
+	}	
+	
+	@Test
+	public void learnToQueryWithRowBounds() throws Exception {
+		// TODO: to move to the more idiomatic way of using sessions, create
+		//       a session here and use a try/finally to close it when done
+		SqlSession session = null;
+		try {
+			session = sessionFactory.openSession();
+
+			int offset = 21;
+			int limit = 12;
+			RowBounds rb = new RowBounds(offset, limit);
+			List<Country> lc = session.selectList("getCountries", null, rb);
+
+			assertEquals(12, lc.size());
+			Country finland = lc.get(11);
+			assertEquals("Finland", finland.getCountry());
+			
+		} finally {
+			if (session != null) session.close();
+		}
+	}		
+	
+	@Test
+	public void learnToQueryWithRowBoundsToMapperClass() throws Exception {
+		// TODO: to move to the more idiomatic way of using sessions, create
+		//       a session here and use a try/finally to close it when done
+		SqlSession session = null;
+		try {
+			session = sessionFactory.openSession();
+
+			int offset = 21;
+			int limit = 12;
+			RowBounds rb = new RowBounds(offset, limit);
+			Koan05Mapper mapper = session.getMapper(Koan05Mapper.class);
+			List<Country> lc = mapper.getCountries(rb);
+
+			assertEquals(12, lc.size());
+			Country finland = lc.get(11);
+			assertEquals("Finland", finland.getCountry());
+			
+		} finally {
+			if (session != null) session.close();
+		}
+	}		
+}
