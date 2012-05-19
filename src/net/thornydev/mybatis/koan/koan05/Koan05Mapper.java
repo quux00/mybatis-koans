f034ad668d01fdc6e3d25eed83f80e650ff95fdf
Michael Peterson <mpeterson2@gmail.com>
2012-05-18 23:43:48 -0400

    Found parts of koan05 that had not been stripped.
    Now corrected and ready for use.

--- src/net/thornydev/mybatis/koan/koan05/Koan05Mapper.java
+++ src/net/thornydev/mybatis/koan/koan05/Koan05Mapper.java
@@ -9,9 +9,9 @@ import net.thornydev.mybatis.koan.domain.Country;
 
 public interface Koan05Mapper {
 	// TODO: this method should take a parameter in order to query by id
-	Country getCountryById(int id);
+	Country getCountryById();
 	// TODO: add method to call the "getAllCountries" statement in the koan05-mapper.xml file
-	List<Country> getAllCountries();
+  // ... method here ...
 	// TODO: add an annotation to getAllCountriesMappedById that will make it work
 	@MapKey("id")
 	Map<Integer,Country> getAllCountriesMappedById();
