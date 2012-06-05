6668ba8dbf65956e9a21eee94eb207da5620805e
Michael <mpeterson2@gmail.com>
2012-06-04 20:20:13 -0400

    Koan21 completed and working, ready to be stripped.

--- src/net/thornydev/mybatis/koan/domain/Film.java
+++ src/net/thornydev/mybatis/koan/domain/Film.java
@@ -27,7 +27,7 @@ public class Film {
 
   // skipping "full text" column (or table) since it is treated differently
   // in the different databases
-
+  
   public Film() {}
 
   public Film(int id) {
@@ -120,5 +120,21 @@ public class Film {
 
   public void setSpecialFeatures(List<String> specialFeatures) {
     this.specialFeatures = specialFeatures;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (! (o instanceof Film)) return false;
+    Film other = (Film)o;
+    
+    return this.id == other.id &&
+        this.rentalDuration == other.rentalDuration &&
+        this.length == other.length &&
+        ((this.title == other.title) || (this.title.equals(other.title))) &&
+        ((this.description == other.description) || (this.description.equals(other.description))) &&
+        ((this.rating == other.rating) || (this.rating.equals(other.rating))) &&
+        ((this.lastUpdate == other.lastUpdate) || (this.lastUpdate.equals(other.lastUpdate))) &&
+        ((this.rentalRate == other.rentalRate) || (this.rentalRate.compareTo(other.rentalRate)) == 0) &&
+        ((this.replacementCost == other.replacementCost) || (this.replacementCost.compareTo(other.replacementCost)) == 0);
   }
 }
