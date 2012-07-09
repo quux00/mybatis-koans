52fcdb09fc7f91c8fcd26fe9ef4818f5836a0308
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2012-07-09 08:32:37 -0500

    a

--- src/test/java/net/thornydev/mybatis/test/koan21/Koan21Mapper.java
+++ src/test/java/net/thornydev/mybatis/test/koan21/Koan21Mapper.java
@@ -8,10 +8,17 @@ import net.thornydev.mybatis.test.domain.Film;
 import org.apache.ibatis.annotations.Param;
 
 public interface Koan21Mapper {
-  List<String> selectLongFilmsByTitleOrRating(Film f);
-  List<String> selectFilmByTitleRatingAndOrMinLength(Film f);
-  List<String> selectFilmByRentalRates(double[] rates);
-  List<String> selectFilmByRentalRates2(@Param("rates") List<BigDecimal> rates);
-  void updateFilmIfNecessary(Film f);
-  Film getFilmById(int id);
+
+	List<String> selectLongFilmsByTitleOrRating(Film f);
+
+	List<String> selectFilmByTitleRatingAndOrMinLength(Film f);
+
+	List<String> selectFilmByRentalRates(double[] rates);
+
+	List<String> selectFilmByRentalRates2(@Param("rates") List<BigDecimal> rates);
+
+	void updateFilmIfNecessary(Film f);
+
+	Film getFilmById(int id);
+
 }
