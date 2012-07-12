a02e14f706881ead007d6f28ecab548c9af621d5
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2012-07-12 08:44:28 -0500

    more about junit categories

--- src/test/java/util/FlavorH2.java
+++ src/test/java/util/FlavorH2.java
@@ -1,6 +1,12 @@
 package util;
 
-/** use this annotation on tests that are H2 specific */
+/**
+ * use this annotation on tests that are H2 specific
+ * 
+ * <a href=
+ * "http://kentbeck.github.com/junit/javadoc/latest/org/junit/experimental/categories/Categories.html"
+ * >more about junit categories</a>
+ */
 public @interface FlavorH2 {
 
 }
--- src/test/java/util/FlavorMySQL.java
+++ src/test/java/util/FlavorMySQL.java
@@ -1,6 +1,12 @@
 package util;
 
-/** use this annotation on tests that are MySQL specific */
+/**
+ * use this annotation on tests that are MySQL specific
+ * 
+ * <a href=
+ * "http://kentbeck.github.com/junit/javadoc/latest/org/junit/experimental/categories/Categories.html"
+ * >more about junit categories</a>
+ */
 public @interface FlavorMySQL {
 
 }
