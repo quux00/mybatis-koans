a2e82c1dffe99911f5ec2ef0f98bee5392a4f3a7
Michael Peterson <mpeterson2@gmail.com>
2012-07-25 18:22:14 -0400

    Restructured ant maven scriptlets.
    Scripts dir now has comp and main dirs, within each of
    which is a db dir and then ant scriptlets for running
    maven in each of those db dirs.

---
+++ scripts/helper/buildMavenTargs.groovy
@@ -0,0 +1,35 @@
+#!/usr/bin/env groovy
+
+// Script to generate the .ant targets in the build directory
+//   -> one file for each database flavor and one for each koan
+// If you add a different database flavor or a new koan, just
+// edit the dbs and koans variables at the top of the file.
+
+def dbs = ['h2', 'mysql', 'pg'];
+def koans = 1 .. 23;
+
+def tmplFile = new File('maven-run.ant.tmpl');
+
+dbs.each { db ->
+  koans.each { k ->
+    def n = leftPad(k)
+    def flist = [new File("../comp/${db}/maven-run-comp-koan${n}-${db}.ant"),
+                 new File("../main/${db}/maven-run-main-koan${n}-${db}.ant")]
+    flist.each { f ->
+      f.withPrintWriter { w ->
+        tmplFile.eachLine { line ->
+          line = line.replaceAll(~/run-comp-koans-h2/,
+                                 "run-comp-koans-${db}")
+          line = line.replaceAll(~/koan=Koan01/,
+                                 "koan=Koan${n}")
+          w.println(line)
+        }
+      }
+    }
+  }
+}
+
+def leftPad(n) {
+  if (n < 10) return "0" + n.toString()
+  else        return n.toString()
+}
